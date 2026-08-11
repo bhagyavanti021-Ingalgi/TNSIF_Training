297769e66dcc8b29178c02e18695f72a7b51fdd9
bhagyavanti144 <bhagyavanti144@gmail.com>
2026-08-11 12:00:39 +0530

    11 commit

---
+++ TNSIF_Training/src/org/tnsif/acce/c2tc/interfacedemo/functionalInterfaceDemo.java
@@ -0,0 +1,14 @@
+package org.tnsif.acce.c2tc.interfacedemo;
+
+public class functionalInterfaceDemo {
+
+	public static void main(String[] args) {
+		Notification email=new EmailNotification();
+		email.notifyUser("Meeting at 10am");
+		
+		Notification sms=new SMSNotification();
+		sms.notifyUser("Assignment uploaded");
+		
+	}
+
+}

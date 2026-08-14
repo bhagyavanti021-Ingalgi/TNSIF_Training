3c8e06b1b7929ad6c24c51ef97f692d21a1f3281
bhagyavanti144 <bhagyavanti144@gmail.com>
2026-08-14 12:41:37 +0530

    first commit

---
+++ TNSIF_Training/src/org/tnsif/acc/c2tc/excetptionhandling/StringIndexOutOfBoundExceptionDemo.java
@@ -0,0 +1,17 @@
+package org.tnsif.acc.c2tc.excetptionhandling;
+
+public class StringIndexOutOfBoundExceptionDemo {
+
+	public static void main(String[] args) {
+		
+				String str="hello";
+				try {
+					char ch=str.charAt(5);
+					System.out.println(ch);
+				}catch(StringIndexOutOfBoundsException e)
+				{
+					System.out.println(e);
+				}	
+	}
+
+}
